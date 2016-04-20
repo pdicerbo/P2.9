95a67ed684353cbcd3ad8d2794b8246da4f2e060
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-04-20 11:57:22 +0200

    almost done

--- D1-exercise/src/MyRNG.cpp
+++ D1-exercise/src/MyRNG.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -65,21 +66,57 @@ double MyRNG::my_lcg(){
   return ((double) next) / ((double) m_);
 }
 
+void print_output_file(string out, int N, double* dat){
+
+  char* NameFile = &out[0];
+  ofstream f(NameFile);
+
+#ifdef __DEBUG
+  cout << "Within print_output_file; namefile is " << out << endl;
+#endif
+  
+  if(f.is_open()){
+    
+    for(int j = 0; j < N - 3; j++){
+      for(int k = 0; k < 3; k++)
+	f << dat[j + k] << "\t";
+      f << endl;
+    }
+    
+    // f << N << " ciao\n";
+
+    f.close();
+  }
+  else
+    cout << "Unable to open file\n";
+}
+
 int main(int argc, char** argv){
 
   int m = 2147483647;
   int a = 16807;
-  // int Nrand = 10000;
-  int Nrand = 10;
-  MyRNG rng(a, m);
-  double myrand;
+  int Nrand = 10000;
+  // int Nrand = 10;
+  double* myrand = new double[Nrand];
+  double correlation = 0.;
   
+  MyRNG rng(a, m);
   rng.set_seed(1234);
 
-  for(int j = 0; j < Nrand; j++){
-    myrand = rng.my_lcg();
-    cout << "Random number " << j << " is " << myrand << endl;
-  }  
+  for(int j = 0; j < Nrand; j++)
+    myrand[j] = rng.my_lcg();
+
+  // calculate correlation
+  for(int j = 0; j < Nrand-2; j++)
+    correlation += myrand[j]*myrand[j+1];
+
+  correlation /= Nrand;
+
+  cout << "the correlation obtained for the obtained random sequence is: " << correlation << endl;
+
+  print_output_file("RandLCG.dat", Nrand, myrand);
+  
+  delete [] myrand;
 
   return 0;
 }
